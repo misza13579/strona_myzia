f72f74a1a5c3d06ed3fd0755995a7ffff5ec3239
misza13579 <131401206+misza13579@users.noreply.github.com>
2025-01-22 12:54:54 +0100

    Refactor Points component to fetch and display user points from production API

--- src/components/points.js
+++ src/components/points.js
@@ -4,11 +4,36 @@ import axios from 'axios';
 
 const Points = () => {
 
-    const [points, setPoints] = useState(null);
+    const [points_myzia, setPoints_myzia] = useState(null);
+    const [points_myzio, setPoints_myzio] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            // Wysyłamy zapytanie z tokenem w nagłówku
+            axios.get(`https://strona-myzia-backend-production.up.railway.app/points`, {headers: {
+                  Authorization: `Bearer ${token}`, 
+                },})
+              .then((response) => {
+                setPoints_myzio(response.data.myzio);
+                setPoints_myzia(response.data.myzia);  // Ustawiamy dane użytkownika
+              })
+              .catch((error) => {
+                console.error('Błąd autoryzacji', error);
+              });
+          } else {
+            console.log('Nie ma tokenu');
+          }
+        }, []);
+      
+        if (!user) return <p>Ładowanie...</p>;
     return (
-        <h1>Licznik punktów</h1>
         <div>
-            
+            <h1>Licznik punktów</h1>
+            <div>
+                <p>das</p>
+            </div>
         </div>
     );
 };
