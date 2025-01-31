466b2aafd49469e9acf329966dbaa77368bd556e
misza13579 <131401206+misza13579@users.noreply.github.com>
2025-01-31 13:45:27 +0100

    Display connection status in Points component header

--- src/components/points.js
+++ src/components/points.js
@@ -109,7 +109,7 @@ const Points = () => {
 
           <div className="flex items-center justify-center col-span-1">
               <div className="bg-red-200 h-32 w-64 rounded flex items-center justify-center">
-                  <p className="font-medium text-4xl">Licznik punktów</p>
+                  <p className="font-medium text-4xl">Licznik punktów {connected}</p>
               </div>
           </div>
 
